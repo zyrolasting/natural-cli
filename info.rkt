188d1ce4b943f2a46c00c44e6ec52ceb12bf4ab2
Sage Gerard <sage@sagegerard.com>
2019-11-18 09:56:46 -0500

    Add compatibility-lib dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "natural-cli")
-(define deps '("base"))
+(define deps '("base" "compatibility-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/natural-cli.scrbl" ())))
 (define pkg-desc "Build non-trivial CLIs with little effort.")
